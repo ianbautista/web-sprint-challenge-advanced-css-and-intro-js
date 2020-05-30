83a9f756aa9bc54dac8fdddfd47be9178fe45739
ianbautista <ianfbautista@gmail.com>
2020-05-29 21:06:00 -0700

    task5 done

--- index.js
+++ index.js
@@ -275,16 +275,24 @@ get20s(artists);
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
- * removeArtist removes an artist from the array at the index and console.logs the length of the remaining dataset.
+ * removeArtist removes an artist from the array at the index and console.logs the length of the
+ * remaining dataset.
  *
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset and log the number 19.
  *
- * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.
+ * Note that sucessfully invoking this function multiple times without refreshing your browser will
+ * continuously remove artists from the array until there are none left. If you refresh your browser,
+ * the data will reset.
  */
-function removeArtist(/*code here*/) {
+function removeArtist(artistsArray, index) {
 	/* code here */
+	if (index > -1) {
+		artistsArray.splice(index, 1);
+	}
+	console.log(artistsArray.length);
 }
+removeArtist(artists, 0);
 
 /**
 
